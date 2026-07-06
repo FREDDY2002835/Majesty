49310523ba9f7dda9239494e2eba559213573deb
Frederick Muhimuzi <muhimuzifreddy059@gmail.com>
2026-07-06 15:45:21 +0300

    database

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://majesty-backend-inux.onrender.com/api';
 
 // Helper: get token from localStorage
 const getToken = () => localStorage.getItem('token');
