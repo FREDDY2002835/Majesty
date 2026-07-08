da8bc3cdba8df9ffd7266884f9117709b2dd7924
Frederick Muhimuzi <muhimuzifreddy059@gmail.com>
2026-07-08 12:00:34 +0300

    api

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = 'https://majesty-backend-inux.onrender.com';
 
 // Helper: get token from localStorage
 const getToken = () => localStorage.getItem('token');
